1beb1ebe2cbd36e487253327ec6bf28c26456de0
karim <karimkarim20444@gmail.com>
2025-04-03 11:55:23 -0700

    4/3

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -33,7 +33,7 @@ const HomeInfo = ({ currentStage }) => {
     return (
       <div className='info-box'>
         <p className='font-medium text-center sm:text-xl'>
-        Having successfully led multiple projects to success over the years, I'm eager to showcase the tangible impact of my work. <br /> Explore my portfolio to witness the results of my expertise and the value I can bring to your projects.
+        Over the years, I've successfully led multiple projects to completion, delivering tangible results. <br /> Feel free to explore my portfolio and see the value I can bring to your projects.
         </p>
 
         <Link to='/projects' className='neo-brutalism-white neo-btn'>
@@ -48,7 +48,7 @@ const HomeInfo = ({ currentStage }) => {
     return (
       <div className='info-box'>
       <p className='font-medium sm:text-xl text-center'>
-        Need a project done or looking for a dev? <br/> I'm just a few keystrokes away
+      Need a project done or looking for a developer? <br/> I'm just a message away! 🚀
       </p>
 
       <Link to='/contact' className='neo-brutalism-white neo-btn'>
