5359c34a21f8a31fb6b42bb14c53739f56541927
karim <karimkarim20444@gmail.com>
2025-04-03 11:15:44 -0700

    4/3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,128 @@
+// import { Canvas } from "@react-three/fiber";
+// import { Suspense, useState } from "react";
+
+// /* import sakura from "../assets/sakura.mp3"; */ // this if add sound
+// import { HomeInfo, Loader } from "../components";
+// import { Bird, Island, Plane, Sky } from "../models";
+
+// const Home = () => {
+//   const [currentStage, setCurrentStage] = useState(1);
+//   const [isRotating, setIsRotating] = useState(false);
+
+//   const adjustBiplaneForScreenSize = () => {
+//     let screenScale, screenPosition;
+
+//     // If screen width is less than 768px, adjust the scale and position
+//     if (window.innerWidth < 768) {
+//       screenScale = [1.5, 1.5, 1.5];
+//       screenPosition = [0, -1.5, 0];
+//     } else {
+//       screenScale = [3, 3, 3];
+//       screenPosition = [0, -4, -4];
+//     }
+
+//     return [screenScale, screenPosition];
+//   };
+
+//   const adjustIslandForScreenSize = () => {
+//     let screenScale, screenPosition;
+
+//     if (window.innerWidth < 768) {
+//       screenScale = [0.7, 0.7, 0.7];
+//       screenPosition = [0, -6.5, -43.4];
+//     } else {
+//       screenScale = [1, 1, 1];
+//       screenPosition = [0, -6.5, -43.4];
+//     }
+
+//     return [screenScale, screenPosition];
+//   };
+
+//   const [biplaneScale, biplanePosition] = adjustBiplaneForScreenSize();
+//   const [islandScale, islandPosition] = adjustIslandForScreenSize();
+
+//   return (
+//     <section className="w-full h-screen relative">
+//       <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+//         {currentStage && <HomeInfo currentStage={currentStage} />}
+//       </div>
+
+//       <Canvas
+//         className={`w-full h-screen bg-transparent ${
+//           isRotating ? "cursor-grabbing" : "cursor-grab"
+//         }`}
+//         camera={{ near: 0.1, far: 1000 }}
+//       >
+//         <Suspense fallback={<Loader />}>
+//           <directionalLight position={[3, 1, 1]} intensity={2} />
+//           <ambientLight intensity={0.5} />
+//           <pointLight position={[10, 5, 10]} intensity={2} />
+//           <spotLight
+//             position={[0, 50, 10]}
+//             angle={0.15}
+//             penumbra={1}
+//             intensity={2}
+//           />
+//           <hemisphereLight
+//             skyColor="#b1e1ff"
+//             groundColor="#000000"
+//             intensity={1}
+//           />
+
+//           <Bird />
+//           <Sky isRotating={isRotating} />
+//           <Island
+//             isRotating={isRotating}
+//             setIsRotating={setIsRotating}
+//             setCurrentStage={setCurrentStage}
+//             position={islandPosition}
+//             rotation={[0.1, 4.7077, 0]}
+//             scale={islandScale}
+//           />
+//           <Plane
+//             isRotating={isRotating}
+//             position={biplanePosition}
+//             rotation={[0, 20.1, 0]}
+//             scale={biplaneScale}
+//           />
+//         </Suspense>
+//       </Canvas>
+
+//       <div className="hand-enimation">
+//         <div className="blue-gradient_text drop-shadow conte-ani-hand">
+//           move it
+//         </div>
+//         <div className="hand-anii">✋🏻</div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Home;
+
+
+
 import { Canvas } from "@react-three/fiber";
-import { Suspense, useEffect, useRef, useState } from "react";
- 
+import { Suspense, useState, useEffect } from "react";
+
 /* import sakura from "../assets/sakura.mp3"; */ // this if add sound
 import { HomeInfo, Loader } from "../components";
-import { soundoff, soundon } from "../assets/icons";
 import { Bird, Island, Plane, Sky } from "../models";
 
 const Home = () => {
-/*    const audioRef = useRef(new Audio(sakura));  // this if add sound
-  audioRef.current.volume = 0.4;
-  audioRef.current.loop = true; */ 
-
   const [currentStage, setCurrentStage] = useState(1);
   const [isRotating, setIsRotating] = useState(false);
-  const [isPlayingMusic, setIsPlayingMusic] = useState(false);
-/*  // this if add sound
-  useEffect(() => { 
-    if (isPlayingMusic) {
-      audioRef.current.play();
-    }
+  const [scrollPosition, setScrollPosition] = useState(0);
 
-    return () => {
-      audioRef.current.pause();
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
     };
-  }, [isPlayingMusic]); */
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const adjustBiplaneForScreenSize = () => {
     let screenScale, screenPosition;
@@ -58,8 +157,8 @@ const Home = () => {
   const [islandScale, islandPosition] = adjustIslandForScreenSize();
 
   return (
-    <section className='w-full h-screen relative'>
-      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+    <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         {currentStage && <HomeInfo currentStage={currentStage} />}
       </div>
 
@@ -80,8 +179,8 @@ const Home = () => {
             intensity={2}
           />
           <hemisphereLight
-            skyColor='#b1e1ff'
-            groundColor='#000000'
+            skyColor="#b1e1ff"
+            groundColor="#000000"
             intensity={1}
           />
 
@@ -94,31 +193,23 @@ const Home = () => {
             position={islandPosition}
             rotation={[0.1, 4.7077, 0]}
             scale={islandScale}
+            scrollPosition={scrollPosition}
           />
           <Plane
             isRotating={isRotating}
             position={biplanePosition}
             rotation={[0, 20.1, 0]}
             scale={biplaneScale}
+            scrollPosition={scrollPosition}
           />
         </Suspense>
       </Canvas>
 
-{/* {      <div className='absolute bottom-2 left-2'> // this if add sound
-        <img
-          src={!isPlayingMusic ? soundoff : soundon}
-          alt='jukebox'
-          onClick={() => setIsPlayingMusic(!isPlayingMusic)}
-          className='w-10 h-10 cursor-pointer object-contain'
-        />
-      </div>} */}
       <div className="hand-enimation">
-      <div className="blue-gradient_text drop-shadow conte-ani-hand">
-      move it
-      </div>
-      <div className="hand-anii">
-      ✋🏻
-      </div>
+        <div className="blue-gradient_text drop-shadow conte-ani-hand">
+          move it
+        </div>
+        <div className="hand-anii">✋🏻</div>
       </div>
     </section>
   );
