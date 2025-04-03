47a4e7f3b192b3cb690ddcdbedab88be45f9c71a
karim <karimkarim20444@gmail.com>
2025-04-03 13:19:49 -0700

    4/3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,19 +7,18 @@ const Navbar = () => {
   return (
     <header className='header'>
       <NavLink to='/'>
-        {/* <img src={logo} alt='logo' className='w-18 h-18 object-contain' /> */}
         <span className='blue-gradient_text drop-shadow font-semibold font-semibold-logo '>
           KE
         </span>
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium header-nav-a-container'>
-        <NavLink to='/about' className={ (  { isActive }) => isActive ? "text-white "   : "text-gray-600 "   } >
+        <NavLink to='/about' className={ (  { isActive }) => isActive ? "text-white a-fucase "   : "text-gray-600 "   } >
           About
         </NavLink>
-        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-white" : "text-gray-600"}>
+        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-white a-fucase" : "text-gray-600"}>
           Projects
         </NavLink>
-        <NavLink to='/contact' className={({ isActive }) => isActive ? "text-white"  : "text-gray-600 "}>
+        <NavLink to='/contact' className={({ isActive }) => isActive ? "text-white a-fucase"  : "text-gray-600 "}>
           Contact
         </NavLink>
       </nav>
